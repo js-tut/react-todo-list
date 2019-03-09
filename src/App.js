a866841876df347bebb375b49bc9ab24b0f1f831
js-tut <johnsmilga@gmail.com>
2019-03-08 23:43:33 -0800

    ItemList Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-// showing vs-code github setup
 class App extends Component {
   state = {
     items: [],
